77cfc0eb6c9564b88eba559fbbbb4f5ca5c17817
Huixies <1062240360@qq.com>
2021-01-27 14:45:50 +0800

    更新

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+rm -rf .git &&
+git init &&
+git add . &&
+git commit -m "更新" &&
+git branch -M master &&
+git remote add origin git@github.com:Huixies/react-demo.git &&
+git push -f -u origin master &&
+cd -
+echo https://github.com/Huixies/react-demo/#/
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = ({icon,title})=> {
+    return (
+        <nav className='navbar bg-primary'>
+            <h1>
+                <i className={icon}>
+                &nbsp; {title}
+                </i>
+            </h1>
+            <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/about">About</Link>
+                </li>
+            </ul>
+        </nav>
+    );   
+}
+//设置默认属性值
+Navbar.defaultProps = {
+  title: 'Github Finder',
+  icon: 'fa fa-github'
+}
+
+// 定义属性类型
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired
+}
+
+export default Navbar;
